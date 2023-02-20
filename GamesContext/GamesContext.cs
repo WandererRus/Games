095b5c48dbfe64d2fe952065b36c22e6ddb922e0
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-02-20 19:30:21 +0300

    Добавьте файлы проекта.

---
+++ GamesContext/GamesContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GamesDll;
+
+namespace GamesContext
+{
+    public class GamesContext : DbContext
+    {
+
+        public DbSet<Games> gamesSet { get; set; }
+
+        public GamesContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        public GamesContext(string nameOrConnectionString, DbCompiledModel model) : base(nameOrConnectionString, model)
+        {
+        }
+
+        public GamesContext(DbConnection existingConnection, bool contextOwnsConnection) : base(existingConnection, contextOwnsConnection)
+        {
+        }
+
+        public GamesContext(ObjectContext objectContext, bool dbContextOwnsObjectContext) : base(objectContext, dbContextOwnsObjectContext)
+        {
+        }
+
+        public GamesContext(DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection) : base(existingConnection, model, contextOwnsConnection)
+        {
+        }
+
+        protected GamesContext()
+        {
+        }
+
+        protected GamesContext(DbCompiledModel model) : base(model)
+        {
+        }
+    }
+}
